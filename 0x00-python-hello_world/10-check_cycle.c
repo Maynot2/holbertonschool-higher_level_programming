b1e78b9768f75483c7cf468bcc91a074f01db082
Maynot2 <paulmanot@gmail.com>
2020-12-07 19:12:50 +0000

    Create function to detacte cycle in link lists

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - Checks if cycle are present in linked list.
+ * @list: A list.
+ *
+ * Return: 1 if cycle found.
+ *         0 if not found.
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *hare;
+	listint_t *tortoise;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	tortoise = list;
+	hare = list->next;
+
+	while (hare->next)
+	{
+		if (tortoise == hare)
+			return (1);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return (0);
+}
