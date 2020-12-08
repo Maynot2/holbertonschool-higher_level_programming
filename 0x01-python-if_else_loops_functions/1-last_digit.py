9e1a5b51054068a241470a0e58bcaeee81da4e6d
Maynot2 <paulmanot@gmail.com>
2020-12-08 13:08:21 +0000

    Take care of case when number equal zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = int(str(number)[-1]) * abs(number)//number
+number = 0
 
-if last_digit > 5:
-    msg = "greater than 5"
-elif last_digit == 0:
-    msg = "0"
+if number == 0:
+    print("Last digit of {} is 0".format(number))
 else:
-    msg = "less than 6 and not 0"
+    last_digit = int(str(number)[-1]) * abs(number)//number
 
-print("Last digit of {} is {} and is {}".format(number, last_digit, msg))
+    if last_digit > 5:
+        msg = "greater than 5"
+    elif last_digit == 0:
+        msg = "0"
+    else:
+        msg = "less than 6 and not 0"
+
+    print("Last digit of {} is {} and is {}".format(number, last_digit, msg))
