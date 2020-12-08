aa53b404a3c31021ff07c4fa1380a3e637f564d2
Maynot2 <paulmanot@gmail.com>
2020-12-08 13:38:00 +0000

    Change function name

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-def islower(c):
+def _islower(c):
     return 97 <= ord(c) <= 122
 
 
 def uppercase(str):
     str += "\n"
     for char in str[:]:
-        if islower(char):
+        if _islower(char):
             print(chr(ord(char) - 32), end="")
         else:
             print(char, end="")
