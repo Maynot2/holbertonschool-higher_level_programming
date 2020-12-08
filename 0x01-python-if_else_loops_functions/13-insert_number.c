4b84d0dbf01662317fc18554f6d2d08d2644e3d0
Maynot2 <paulmanot@gmail.com>
2020-12-08 19:57:23 +0000

    Change logic

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,40 +7,40 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *after = NULL;
 	listint_t *node = NULL;
 
-	if (!head)
-		return (NULL);
-	
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
 	node->n = number;
 	node->next = NULL;
 
-	if (*head)
-		current = *head;
-
-	if ((*head)->next)
-		after = current->next;
-
-	while(current)
-	{
-		if(current->n < node->n && current->next == NULL)
-		{
-			current->next = node;
-			break;
-		}
-		if((current->n < node->n) && (after->n > node->n))
-		{
-			node->next = after;
-			current->next = node;
-		}
-		current = current->next;
-		if(after->next)
-			after = after->next;
-	}
-
 	if (*head == NULL)
 		*head = node;
-
+	else
+	{
+		if ((*head)->next == NULL && node->n > (*head)->n)
+		{
+			(*head)->next = node;
+			return (node);
+		}
+		current = *head;
+		if (current->next)
+			after = current->next;
+		while (current)
+		{
+			if(current->n < node->n && current->next == NULL)
+			{
+				current->next = node;
+				break;
+			}
+			if((current->n < node->n) && (after->n > node->n))
+			{
+				node->next = after;
+				current->next = node;
+			}
+			current = current->next;
+			if(after->next)
+				after = after->next;
+		}
+	}
 	return (node);
 }
