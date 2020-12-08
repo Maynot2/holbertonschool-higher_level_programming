6cf0c64ae8ce0c175d4fe149c9c42796aa15a1af
Maynot2 <paulmanot@gmail.com>
2020-12-08 19:34:25 +0000

    Add node when only one element

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,29 +7,40 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *after = NULL;
 	listint_t *node = NULL;
 
-	if (*head && (*head)->next)
-	{
+	if (!head)
+		return (NULL);
+	
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+	node->n = number;
+	node->next = NULL;
+
+	if (*head)
 		current = *head;
+
+	if ((*head)->next)
 		after = current->next;
-		node = malloc(sizeof(listint_t));
-		if (!node)
-			return (NULL);
-		node->n = number;
-		node->next = NULL;
-	}
 
 	while(current)
 	{
+		if(current->n < node->n && current->next == NULL)
+		{
+			current->next = node;
+			break;
+		}
 		if((current->n < node->n) && (after->n > node->n))
 		{
 			node->next = after;
 			current->next = node;
 		}
-		if(current->n < node->n && current->next == NULL)
-			current->next = node;
 		current = current->next;
 		if(after->next)
 			after = after->next;
 	}
+
+	if (*head == NULL)
+		*head = node;
+
 	return (node);
 }
