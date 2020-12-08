c6c094fa7a6a1d47bad1cb5df58dcb79836f37b6
Maynot2 <paulmanot@gmail.com>
2020-12-08 14:36:47 +0000

    Use modulo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number == 0:
-    print("Last digit of {} is 0".format(number))
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -last_digit
+
+if last_digit > 5:
+    msg = "greater than 5"
+elif last_digit == 0:
+    msg = "0"
 else:
-    last_digit = int(str(number)[-1]) * abs(number)//number
+    msg = "less than 6 and not 0"
 
-    if last_digit > 5:
-        msg = "greater than 5"
-    elif last_digit == 0:
-        msg = "0"
-    else:
-        msg = "less than 6 and not 0"
-
-    print("Last digit of {} is {} and is {}".format(number, last_digit, msg))
+print("Last digit of {} is {} and is {}".format(number, last_digit, msg))
