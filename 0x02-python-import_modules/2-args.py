f84213cfb461fcd6a7eee65379c0730ed54ed611
Maynot2 <paulmanot@gmail.com>
2020-12-10 12:40:01 +0000

    Remove extra space in formated string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif (argc == 2):
         print("1 argument:")
-        print("1: {} ".format(argv[1]))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(argc - 1))
         for i in range(len(argv)):
             if i != 0:
-                print("{}: {} ".format(i, argv[i]))
+                print("{}: {}".format(i, argv[i]))
