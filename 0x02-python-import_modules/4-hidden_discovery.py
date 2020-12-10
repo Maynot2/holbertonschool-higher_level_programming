7da732fb4360b9cd97465cd2766ea6c9372e3754
Maynot2 <paulmanot@gmail.com>
2020-12-10 10:53:05 +0000

    Create script to print function names from hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    elements = dir(hidden_4)
+
+    for el in elements:
+        if el[0] != "_" and el[1] != "_":
+            print(el);
