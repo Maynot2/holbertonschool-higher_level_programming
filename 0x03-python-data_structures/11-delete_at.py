c3fda278cb702c2b696fdcbd2a7d225efe24d745
Maynot2 <paulmanot@gmail.com>
2020-12-15 15:59:59 +0000

    Return original list instead of copy

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,5 @@
 def delete_at(my_list=[], idx=0):
     if my_list:
         if 0 <= idx < len(my_list):
-            return my_list[:idx] + my_list[idx+1:]
-        else:
-            return my_list
+            my_list = my_list[:idx] + my_list[idx+1:]
+        return my_list
