cda3179d2f42fd7be422defff18a0007f0df662a
Maynot2 <paulmanot@gmail.com>
2020-12-15 20:11:50 +0000

    Incomplete solution

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * print_python_list_info - Prints info about python
+  * @p: A Pyobject
+  *
+  * Return: Nothing.
+  */
+
+void print_python_list_info(PyObject *p)
+{
+	puts("I am too tired for this!");
+}
