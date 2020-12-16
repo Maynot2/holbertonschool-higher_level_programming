1481367f501a945d2c974553334630f31c7cb9fc
Maynot2 <paulmanot@gmail.com>
2020-12-16 06:29:04 +0000

    Change condition for list presence

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if my_list:
+    if my_list != None:
         if 0 <= idx < len(my_list):
             my_list[:] = my_list[:idx] + my_list[idx+1:]
         return my_list
