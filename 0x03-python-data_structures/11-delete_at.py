b373a3a9e979e7fbbf11adc1222b8f33dfbad6f1
Maynot2 <paulmanot@gmail.com>
2020-12-16 06:30:40 +0000

    Change condition to match pep8 standard

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if my_list != None:
+    if my_list is not None:
         if 0 <= idx < len(my_list):
             my_list[:] = my_list[:idx] + my_list[idx+1:]
         return my_list
