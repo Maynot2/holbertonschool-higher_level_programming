9bfa3fa4a72734bbbab48a478a051ec393163d57
Maynot2 <paulmanot@gmail.com>
2020-12-17 15:13:41 +0000

    Create function complex_delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    keys = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            keys.append(k)
+    for key in keys:
+        a_dictionary.pop(key)
+    return a_dictionary
