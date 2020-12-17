f497b927e613a9b0580c938d10b22ff53f0a465f
Maynot2 <paulmanot@gmail.com>
2020-12-17 14:48:49 +0000

    Create function weight_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0;
+    dictionary = dict(my_list)
+    total = 0
+    for k, v in dictionary.items():
+        total += k * v
+    return total / sum(dictionary.values())
