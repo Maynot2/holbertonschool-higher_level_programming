9ec780d9d28b346ae981a3c31e1651a4be0cb7c5
Maynot2 <paulmanot@gmail.com>
2020-12-17 13:30:35 +0000

    Remove white spaces

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,13 @@
 
 def roman_to_int(roman_string):
     mapping = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     total = 0
     if roman_string is not None or type(roman_string) is not str:
