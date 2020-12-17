11a1ab43a49a0ec63ad5110cb64719ff1042fd2d
Maynot2 <paulmanot@gmail.com>
2020-12-17 11:35:32 +0000

    Remove conditional

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,6 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        a_dictionary[key] = value
-        return a_dictionary
+#if a_dictionary:
+    a_dictionary[key] = value
+    return a_dictionary
