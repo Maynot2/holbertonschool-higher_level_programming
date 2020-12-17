7e9b25910738d7ec92b603f56e7d998f9f265b68
Maynot2 <paulmanot@gmail.com>
2020-12-17 13:25:59 +0000

    Take care of cases when not string or string is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,23 +3,24 @@
 
 def roman_to_int(roman_string):
     mapping = {
-        "I"  : 1,
-        "V"  : 5,
-        "X"  : 10,
-        "L"  : 50,
-        "C"  : 100,
-        "D"  : 500,
-        "M"  : 1000
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
     }
     total = 0
-    last_idx = len(roman_string) - 1
-    for i in range(last_idx):
-        rom_char = roman_string[i]
-        next_rom_char = roman_string[i + 1]
-        if mapping[next_rom_char] > mapping[rom_char]:
-            total -= mapping[rom_char]
-        else:
-            total += mapping[rom_char]
-    rom_char = roman_string[last_idx]
-    total += mapping[rom_char]
+    if roman_string is not None or type(roman_string) is not str:
+        last_idx = len(roman_string) - 1
+        for i in range(last_idx):
+            rom_char = roman_string[i]
+            next_rom_char = roman_string[i + 1]
+            if mapping[next_rom_char] > mapping[rom_char]:
+                total -= mapping[rom_char]
+            else:
+                total += mapping[rom_char]
+        rom_char = roman_string[last_idx]
+        total += mapping[rom_char]
     return total
