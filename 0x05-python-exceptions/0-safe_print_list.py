f96bae3901bd69897026dda6eb301675917d19d1
Maynot2 <paulmanot@gmail.com>
2021-01-04 11:04:52 +0000

    Fix pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
             print(my_list[i], end="")
             if i + 1 == x:
-                print();
+                print()
             i += 1
         except IndexError:
-            print();
+            print()
             break
     return i
