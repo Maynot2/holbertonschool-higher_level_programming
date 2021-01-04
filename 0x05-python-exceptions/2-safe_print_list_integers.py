a623caf30a65c729ae81ca8599acd9207f07cf8a
Maynot2 <paulmanot@gmail.com>
2021-01-04 11:27:06 +0000

    Fix pep8 and print formatting of None and empty list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value), end='')
@@ -7,13 +8,17 @@ def safe_print_integer(value):
         return False
     return True
 
+
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     printed = 0
-    while i < x:
-        if safe_print_integer(my_list[i]):
-            printed += 1
-        if i + 1 == x:
-            print();
-        i += 1
+    if my_list:
+        while i < x:
+            if safe_print_integer(my_list[i]):
+                printed += 1
+            if i + 1 == x:
+                print();
+            i += 1
+    if i == 0:
+        print()
     return printed
