cdb78f86b1eaa8b322d97b5459483d5703816a01
Maynot2 <paulmanot@gmail.com>
2021-01-05 10:48:18 +0000

    Create Square class with private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""This is the documentation for 0-square module"""
+
+
+class Square:
+    """Models a real life square with a size attribute"""
+
+    def __init__(self, size=0):
+        """Constructor method for square instances"""
+
+        self.__size = size
