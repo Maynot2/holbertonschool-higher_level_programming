f879df7fc09542be9783c335148f7d9dea85dc8c
Maynot2 <paulmanot@gmail.com>
2021-01-06 09:18:08 +0000

    Create MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import math
+
+class MagicClass:
+    """
+        This is the MagicClass that helps compute operations on circles
+    """
+
+    def __init__(self, radius):
+        self._MagicClass__radius = 0
+        if type(radius) is not int and  type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+
+    def area(self):
+        return self._MagicClass__radius ** 2 * math.pi
+
+
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
+
+    import dis
+    dis.dis(circumference)
