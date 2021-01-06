0b6c9b369a5bdc847de0739789c2aa0fe5c23a45
Maynot2 <paulmanot@gmail.com>
2021-01-06 09:35:39 +0000

    Add default value to init arg

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,7 @@ import math
 class MagicClass:
     """This is the MagicClass that helps compute operations on circles"""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
