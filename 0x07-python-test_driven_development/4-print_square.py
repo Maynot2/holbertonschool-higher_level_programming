eedfb829d162faf85e4b25cc1063f7334ce05312
Maynot2 <paulmanot@gmail.com>
2021-01-08 11:22:00 +0000

    Create function print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains function(s) to print geometric shapes"""
+
+
+def print_square(size):
+    """Prints a square of '#' symbols given a size"""
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    if size == 0:
+        print()
+    for i in range(size):
+        print('#' * size)
