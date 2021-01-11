631c82db3c30290d7712ea2f53487ae76052f964
Maynot2 <paulmanot@gmail.com>
2021-01-11 17:28:02 +0000

    Incomplete solution

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(q1, q2):
+    if q1[0] == q2[0] or q1[1] == q2[1]:
+        return False
+    if abs(q1[0] - q2[0]) == abs(q1[1]- q2[1]):
+        return False
+    return True
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    try:
+        size = int(sys.argv[1])
+    except:
+        print('N must be a number')
+        sys.exit(1)
+    if size < 4:
+        print('N must be at least 4')
+        sys.exit(1)
