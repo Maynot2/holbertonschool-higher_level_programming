4fd805b8dc61a6b2f6087ad00e65780ef6249cb1
Maynot2 <paulmanot@gmail.com>
2021-01-19 15:47:53 +0000

    Implement Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/binpython3
+"""A module about geometric shapes"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square class inheriting from Rectangle"""
+    def __init__(self, size):
+        """Initializes a square with a given side size"""
+        self.integer_validator('side', size)
+        super().__init__(size, size)
