042835a5403b0eb29fb7f3123c839510a703d472
Maynot2 <paulmanot@gmail.com>
2021-01-19 17:23:57 +0000

    Fix typo

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
-#!/usr/binpython3
+#!/usr/bin/python3
 """Instance helper function(s)"""
+
 
 def is_same_class(obj, a_class):
     """Checks if an object is stricly of given class (not a subclass)"""
