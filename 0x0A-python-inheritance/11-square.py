13d4bd477578f8caea72baba85e96ccced2b4f9b
Maynot2 <paulmanot@gmail.com>
2021-01-19 16:06:29 +0000

    Overide __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/binpython3
+"""A module about geometric shapes"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square class inheriting from Rectangle"""
+    def __init__(self, size):
+        """Initializes a square with a given side size"""
+        self.integer_validator('side', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Overides internal string representation of rectangle object"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
