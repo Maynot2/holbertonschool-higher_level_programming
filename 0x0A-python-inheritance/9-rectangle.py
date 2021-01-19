9e7681382bb619a21c6298a9d6e94f1e725c75df
Maynot2 <paulmanot@gmail.com>
2021-01-19 14:59:07 +0000

    Remove getters/setters

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,34 +9,14 @@ class Rectangle(BaseGeometry):
         """Initializes a rectangle with a given width and length"""
         self.integer_validator('width', width)
         self.integer_validator('height', height)
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Getter method for width attribute"""
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        """Setter method for width attribute"""
         self.__width = width
-
-    @property
-    def height(self):
-        """Getter method for height attribute"""
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        """Setter method for height  attribute"""
         self.__height = height
 
     def __str__(self):
         """Overides internal string representation of rectangle object"""
         return '[{}] {}/{}'.format(type(self).__name__,
-                                   self.width, self.height)
+                                   self.__width, self.__height)
 
     def area(self):
         """Computes the area of a rectangle of a given width and length"""
-        return self.width * self.height
+        return self.__width * self.__height
