31b5fae6ecec48d7a135c11320697b3c3113411d
Maynot2 <paulmanot@gmail.com>
2021-01-19 09:40:48 +0000

    Create class MyList with print_sorted methods and tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/binpython3
+"""Custom list behavior"""
+
+class MyList(list):
+    """Manipulates lists"""
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print(sorted(self))
