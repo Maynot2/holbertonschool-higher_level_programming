c522ab309a9d8cc6ee0fd133eda308c4bc19e2d1
Maynot2 <paulmanot@gmail.com>
2021-01-19 14:52:04 +0000

    Add init method with validation

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/binpython3
+"""A module about geometric shapes"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class inheriting from BaseGeometry"""
+    def __init__(self, width, height):
+        """Imitializes a rectangle with a given width and length"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
