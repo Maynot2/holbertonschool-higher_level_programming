f42366c91b4787ba4a6a2aa9d0d103846a85976c
Maynot2 <paulmanot@gmail.com>
2021-01-19 19:29:12 +0000

    Add area method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,3 +10,7 @@ class Square(Rectangle):
         self.integer_validator('side', size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return super().area()
+
