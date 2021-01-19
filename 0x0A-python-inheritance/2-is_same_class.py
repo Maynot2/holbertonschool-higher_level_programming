ac5a284eabbd9932a59d04ea7d3393566f3f3636
Maynot2 <paulmanot@gmail.com>
2021-01-19 09:50:02 +0000

    Create function is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/binpython3
+"""Instance helper function(s)"""
+
+def is_same_class(obj, a_class):
+    """Checks if an object is stricly of given class (not a subclass)"""
+    return type(obj) == a_class
