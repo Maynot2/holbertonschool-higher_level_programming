42c833df502fd41683406392671cb1dd270fe2f1
Maynot2 <paulmanot@gmail.com>
2021-01-19 16:22:34 +0000

    Creat class MyInt

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/binpython3
+"""A module that tweak the int class"""
+
+
+class MyInt(int):
+    """Overloads some of the default int methods"""
+    def __init__(self, i):
+        """Initializes a MyInt object"""
+        if type(i) == int:
+            self._value = i
+
+    def __eq__(self, other):
+        """Overides == method"""
+        return self._value != other
+
+    def __ne__(self, other):
+        """Overides != method"""
+        return self._value == other
