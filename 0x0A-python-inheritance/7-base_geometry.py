001f1e8e76bed3520d803185cd7e66355e53b8b1
Maynot2 <paulmanot@gmail.com>
2021-01-19 14:52:44 +0000

    Add validator method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/binpython3
+"""A module about geometric shapes"""
+
+
+class BaseGeometry:
+    """Base claas for geometric shapes"""
+    def area(self):
+        """Calculates the area of a given geometric shape"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer, triggers an exception
+            otherwise
+        """
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
