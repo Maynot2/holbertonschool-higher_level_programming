7a125bbaf0c93ab82272861432f95e4fe56dd2c2
Maynot2 <paulmanot@gmail.com>
2021-01-20 22:51:35 +0000

    Imcomplete solution

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Adds all arguments to a Python list, and then save them to a file"""
+import sys
+
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print(line)
