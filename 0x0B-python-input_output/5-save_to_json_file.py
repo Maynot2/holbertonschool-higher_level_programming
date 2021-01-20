32658e556c95c965108f46369d28455d0b18fbb6
Maynot2 <paulmanot@gmail.com>
2021-01-20 11:30:53 +0000

    Remove objet existence conditiopn

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Contains jason file and jason string manipulation function(s)"""
 import json
@@ -5,7 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """A python object as a jason file"""
-    if my_obj and filename:
+    if filename:
         with open(filename, mode='w') as f:
             json.dump(my_obj, f)
-
