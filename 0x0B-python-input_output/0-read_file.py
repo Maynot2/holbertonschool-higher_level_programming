9543f4311b5d96715c5acf1b3b0f06ab28779d0a
Maynot2 <paulmanot@gmail.com>
2021-01-20 07:30:36 +0000

    Create function read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains text file manipulation function(s)"""
+
+
+def read_file(filename=""):
+    """Reads a file with utf8 encoding"""
+    if filename:
+        with open(filename, encoding='utf8') as file:
+            print(file.read(), end='')
