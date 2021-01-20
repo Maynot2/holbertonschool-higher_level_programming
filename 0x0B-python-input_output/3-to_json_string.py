0bd5d472dba7d189efe8269484cd8eaaa3013aa5
Maynot2 <paulmanot@gmail.com>
2021-01-20 08:26:38 +0000

    Create function to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains jason file and jason string manipulation function(s)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Turns a python object into a jason string"""
+    if my_obj:
+        return json.dumps(my_obj)
