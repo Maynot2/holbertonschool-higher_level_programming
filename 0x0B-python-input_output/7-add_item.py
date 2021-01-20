8953d8e05e37a8cec762f6c98981b6f220212ee2
Maynot2 <paulmanot@gmail.com>
2021-01-20 11:23:10 +0000

    Change script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list, and then save them to a file"""
 import sys
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 if __name__ == '__main__':
-    args = sys.argv
-    save_to_json_file(args[1:], 'add_item.json')
+    if len(sys.argv) < 2:
+        args = []
+    else:
+        args = sys.argv[1:]
+    if os.path.isfile('add_item.json'):
+        args = load_from_json_file('add_item.json') + args
+    save_to_json_file(args, 'add_item.json')
     print(load_from_json_file('add_item.json'))
