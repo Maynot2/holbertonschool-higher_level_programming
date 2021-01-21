9c52fff5e638ba5718eb0bf93c0dd3b3d8398319
Maynot2 <paulmanot@gmail.com>
2021-01-21 19:40:26 +0000

    Change logic to feat ex constraints

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,13 +10,23 @@ def print_size_errors(size, error_counts):
     for k, v in sorted(error_counts.items()):
         print('{}: {}'.format(k, v))
 
+
 def is_valid_line(line):
     """Check if line is a valid request"""
     regex = '.+ \d+'
     return not not re.match(regex, line)
 
 
-error_counts = {}
+error_counts = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
 
 if __name__ == '__main__':
@@ -30,7 +40,8 @@ if __name__ == '__main__':
                 size = int(tokens[-1])
                 total_size += size
                 error = tokens[-2]
-                error_counts[error] = error_counts.get(error, 0) + 1
+                if error in error_counts:
+                    error_counts[error] += 1
                 if (count % 10 == 0):
                     print_size_errors(total_size, error_counts)
         print_size_errors(total_size, error_counts)
