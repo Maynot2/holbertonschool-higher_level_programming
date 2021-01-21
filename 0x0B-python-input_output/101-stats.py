989309ddbd467887f84c05a3b12c34af2107d7af
Maynot2 <paulmanot@gmail.com>
2021-01-21 19:48:25 +0000

    Print only if value not 0

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,7 +8,8 @@ def print_size_errors(size, error_counts):
     """Print the size of all error files and a count of the error types"""
     print('File size: {}'.format(size))
     for k, v in sorted(error_counts.items()):
-        print('{}: {}'.format(k, v))
+        if v:
+            print('{}: {}'.format(k, v))
 
 
 def is_valid_line(line):
@@ -40,7 +41,7 @@ if __name__ == '__main__':
                 size = int(tokens[-1])
                 total_size += size
                 error = tokens[-2]
-                if error in error_counts:
+                if error in error_counts.keys():
                     error_counts[error] += 1
                 if (count % 10 == 0):
                     print_size_errors(total_size, error_counts)
