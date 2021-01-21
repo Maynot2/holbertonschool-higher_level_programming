5fb01a5418c857ed96112d18667205a1d6d4b85f
Maynot2 <paulmanot@gmail.com>
2021-01-21 15:14:22 +0000

    reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list, and then save them to a file"""
+"""reads stdin line by line and computes metrics:"""
 import sys
 
 
+def print_size_errors(size, error_counts):
+    """Print the size of all error files and a count of the error types"""
+    print('File size: {}'.format(size))
+    for k, v in sorted(error_counts.items()):
+        print('{}: {}'.format(k, v))
+
+
+error_counts = {}
+
+
 if __name__ == '__main__':
-    for line in sys.stdin:
-        print(line)
+    try:
+        count = 0
+        total_size = 0
+        for line in sys.stdin:
+            count += 1
+            tokens = line.split(' ')
+            if len(tokens) >= 2:
+                size = int(tokens[-1])
+                total_size += size
+                error = tokens[-2]
+                error_counts[error] = error_counts.get(error, 0) + 1
+                if (count % 10 == 0):
+                    print_size_errors(total_size, error_counts)
+        print_size_errors(total_size, error_counts)
+    except KeyboardInterrupt:
+                    print_size_errors(total_size, error_counts)
