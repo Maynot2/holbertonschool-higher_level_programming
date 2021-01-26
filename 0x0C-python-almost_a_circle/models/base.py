386d8cccba1068c175ea37464e13f76abcca39ac
Maynot2 <paulmanot@gmail.com>
2021-01-26 08:57:38 +0000

    Create class method save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
     Contains the Base class for geometric shapes
 """
 
+import json
+
 
 class Base:
     """
@@ -23,3 +25,17 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries, sort_keys=True)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        filename = '{}.json'.format(cls.__name__)
+        with open(filename, mode='w') as f:
+            f.write(cls.to_json_string(list_dicts))
