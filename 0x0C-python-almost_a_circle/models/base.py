ffa9997e9099cc9a26a3348a0294d1e9af00f209
Maynot2 <paulmanot@gmail.com>
2021-01-26 16:04:31 +0000

    Add save_to_file_csv and load_from_file_csv classmethods to Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@
 
 import json
 import os.path
+import csv
 
 
 def is_list_of_dicts(l):
@@ -57,7 +58,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save a list of shapes to a file"""
+        """Save a list of shapes to a JSON file"""
         if cls.is_list_of_shapes(list_objs):
             list_dicts = [obj.to_dictionary() for obj in list_objs]
             filename = '{}.json'.format(cls.__name__)
@@ -81,7 +82,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Create a list from instances from file"""
+        """Create a list of instances from JSON file"""
         list_shapes = []
         filename = '{}.json'.format(cls.__name__)
         if os.path.isfile(filename):
@@ -89,4 +90,49 @@ class Base:
                 shapes = cls.from_json_string(f.read())
                 for shape in shapes:
                     list_shapes.append(cls.create(**shape))
+        return list_shapes
+
+    @staticmethod
+    def is_given_obj_list(objtype, list_objs):
+        for obj in list_objs:
+            if obj.__class__.__name__ != objtype:
+                return False
+        return True
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Save a list of shapes to a CSV file"""
+        filename = '{}.csv'.format(cls.__name__)
+        with open(filename, mode='w') as cvs_file:
+            cvs_writer = csv.writer(cvs_file)
+            if cls.is_given_obj_list('Rectangle', list_objs):
+                fields = ['id', 'width', 'height', 'x', 'y']
+                cvs_writer.writerow(fields)
+                for shape in list_objs:
+                    cvs_writer.writerow([shape.id,
+                                         shape.width,
+                                         shape.height,
+                                         shape.x,
+                                         shape.y])
+            if cls.is_given_obj_list('Square', list_objs):
+                fields = ['id', 'size', 'x', 'y']
+                cvs_writer.writerow(fields)
+                for shape in list_objs:
+                    cvs_writer.writerow([shape.id,
+                                         shape.size,
+                                         shape.x,
+                                         shape.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Create a list of instances from CSV file"""
+        list_shapes = []
+        filename = '{}.csv'.format(cls.__name__)
+        if os.path.isfile(filename):
+            with open(filename) as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                    for keys in row:
+                        row[keys] = int(row[keys])
+                    list_shapes.append(cls.create(**row))
         return list_shapes
