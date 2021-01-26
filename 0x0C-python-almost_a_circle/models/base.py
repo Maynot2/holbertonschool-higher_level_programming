afe50320f65cd1321d5f3adfd15807df4ae7f6b9
Maynot2 <paulmanot@gmail.com>
2021-01-26 09:38:33 +0000

    Check for dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,16 @@
 import json
 
 
+def is_list_of_dicts(self):
+    """Check if passed argument l is a list of dicts"""
+    if type(l) not list:
+        return False
+    for item in l:
+        if type(item) not dict
+            return False
+    return True
+
+
 class Base:
     """
         The base class for geometric shapes
@@ -28,14 +38,26 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if not list_dictionaries:
+        if list_dictionaries == [] or list_dictionaries == None:
             return '[]'
-        else:
+        if is_list_of_dicts(list_dictionaries)
             return json.dumps(list_dictionaries, sort_keys=True)
 
     @classmethod
+    def is_list_of_shapes(cls, l):
+        """Check if passed argument l is a list of shapes"""
+        if type(l) not list:
+            return False
+        for item in l:
+            if not isinstance(item, cls)
+                return False
+        return True
+
+    @classmethod
     def save_to_file(cls, list_objs):
-        list_dicts = [obj.to_dictionary() for obj in list_objs]
-        filename = '{}.json'.format(cls.__name__)
-        with open(filename, mode='w') as f:
-            f.write(cls.to_json_string(list_dicts))
+        """Save a list of shapes to a file"""
+        if cls.is_list_of_shapes(list_objs):
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            filename = '{}.json'.format(cls.__name__)
+            with open(filename, mode='w') as f:
+                f.write(cls.to_json_string(list_dicts))
