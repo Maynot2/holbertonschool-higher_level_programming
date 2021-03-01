58f3ba24e856f5d3302a1f8fd88c3973fe90879b
Maynot2 <paulmanot@gmail.com>
2021-03-01 19:09:42 +0000

    Create script

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displays the average temperature (F) by city ordered by temperature (desc).
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`  WHERE month = 7 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
+WHERE `month`=7
+GROUP BY `city` 
+ORDER BY `avg_temp` DESC
+LIMIT 3;
