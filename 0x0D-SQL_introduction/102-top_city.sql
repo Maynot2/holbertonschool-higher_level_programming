fe5ee9a811f29125650b74d0e0b35e2693acf269
Maynot2 <paulmanot@gmail.com>
2021-03-01 19:50:43 +0000

    Try new query

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,6 @@
 -- Displays the average temperature (F) by city ordered by temperature (desc).
-SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`  WHERE month = 7 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
 SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
-WHERE `month`=7 OR `month=8`
+WHERE `month`>=7 AND `month`<=8
 GROUP BY `city` 
 ORDER BY `avg_temp` DESC
 LIMIT 3;
