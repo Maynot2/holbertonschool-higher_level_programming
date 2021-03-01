e45bee0e485c014c8235106c538dc810bbff14f7
Maynot2 <paulmanot@gmail.com>
2021-03-01 16:55:12 +0000

    Fix script

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
 -- Converts DB to UTF8
-ALTER DATABASE `hbtn_0c_0` SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` MODIFY `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
