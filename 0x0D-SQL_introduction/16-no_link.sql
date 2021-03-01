6a8efc280c53a1f01a2f56fe1f5c25e8294d40ac
Maynot2 <paulmanot@gmail.com>
2021-03-01 15:11:29 +0000

    Change query

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- List row with a name value
-SELECT name, score FROM second_table
-WHERE score IS NOT NULL
-ORDER BY score DESC;
+SELECT `name`, `score` FROM `second_table`
+WHERE `name` IS NOT NULL
+ORDER BY `score` DESC;
