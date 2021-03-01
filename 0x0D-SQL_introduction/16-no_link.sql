c4aa48d08c9f0c4f3e71e5ecdaefb3c4be8cc724
Maynot2 <paulmanot@gmail.com>
2021-03-01 15:08:15 +0000

    Fix script

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,4 @@
 -- List row with a name value
-SELECT `score`, `name` FROM `second_table` 
-WHERE `name` NOT NULL
-GROUP BY `score` 
-ORDER BY `score` DESC;
+SELECT name, score FROM second_table
+WHERE score IS NOT NULL
+ORDER BY score DESC;
