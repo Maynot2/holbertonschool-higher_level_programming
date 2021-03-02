73182bd6f2dedacd34422ed8a86599db6603786d
Maynot2 <paulmanot@gmail.com>
2021-03-02 14:25:16 +0000

    Add comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Shows privileges for user user_0d_1 and user_0d_2
+-- Shows privileges for user user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Shows privileges for user user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
