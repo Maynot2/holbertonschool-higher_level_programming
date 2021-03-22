0cd681f99de381aa7169d1cd2700585778caa73f
Maynot2 <paulmanot@gmail.com>
2021-03-22 13:12:51 +0000

    Fix syntax

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 'use strcit';
 
 const nums = process.argv.slice(2, process.argv.length).reduce((acc, curr) => {
-  if (Number(curr)) acc.push(Number(curr))
+  if (Number(curr)) acc.push(Number(curr));
   return acc;
-}, [])
+}, []);
 
 if (nums.length < 2) {
   console.log(0);
