749626c237216d9db2260045c59f700946ec5277
Maynot2 <paulmanot@gmail.com>
2021-03-22 11:09:17 +0000

    Create function factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+'use strcit';
+
+const x = Number(process.argv[2]);
+
+function factorial (num) {
+  if (NaN !== num && !num || x < 2) return 1;
+  
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(x));
