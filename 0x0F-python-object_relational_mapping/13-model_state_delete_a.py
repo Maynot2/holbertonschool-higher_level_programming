c5c5d8ebe5764ecad4cf730d80b5e8aa7a736ef8
Maynot2 <paulmanot@gmail.com>
2021-03-25 21:07:39 +0000

    Delete all states containing letter

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Simple query all to db
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                                                            usr,
+                                                            pwd,
+                                                            db,
+                                                            pool_pre_ping=True
+                                                        ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.name.like('%a%')).delete(
+                                                    synchronize_session=False)
+
+    session.commit()
+    session.close()
