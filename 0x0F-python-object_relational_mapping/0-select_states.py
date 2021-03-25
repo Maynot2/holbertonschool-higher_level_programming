222deac35a83792cf445188977a0829c78518221
Maynot2 <paulmanot@gmail.com>
2021-03-25 09:05:30 +0000

    Create python script that lists all states from db hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    Lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    creds = sys.argv[1:]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            user=creds[0],
+            passwd=creds[1],
+            db=creds[2]
+    )
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states')
+    rows = cur.fetchall()
+    for row in rows:
+        print('{}'.format(row))
