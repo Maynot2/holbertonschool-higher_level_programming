2ce2263d96ec8f17d265ea70c4c44daf070bc1ba
Maynot2 <paulmanot@gmail.com>
2021-03-25 09:37:02 +0000

    Filter by state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    Lists matching given state from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    creds = sys.argv[1:5]
+    state = sys.argv[-1]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            user=creds[0],
+            passwd=creds[1],
+            db=creds[2]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'".format(state))
+    rows = cur.fetchall()
+    for row in rows:
+        print('{}'.format(row))
