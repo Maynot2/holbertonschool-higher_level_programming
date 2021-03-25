716be691aae6e1d83f7b39e4bc686ee40b4c16e6
Maynot2 <paulmanot@gmail.com>
2021-03-25 10:54:48 +0000

    Add sql injection protection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    Lists matching given state from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+import re
+
+if __name__ == '__main__':
+    creds = sys.argv[1:5]
+    state = sys.argv[-1]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            user=creds[0],
+            passwd=creds[1],
+            db=creds[2]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name= %s", (state, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print('{}'.format(row))
+    cur.close()
+    db.close()
