f31ed5a37299b09d81cca1311336346ef3f7d170
Maynot2 <paulmanot@gmail.com>
2021-03-28 08:28:18 +0000

    Add comment inside class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,8 +5,6 @@
 """
 
 
-import sys
-import MySQLdb
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,6 +12,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ State model"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
