c4ed1ce2d970b7788e54ec581a34fa2ab6ab379c
Maynot2 <paulmanot@gmail.com>
2021-04-08 18:42:22 +0000

    second attempt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-    for i in range(len(list_of_integers)):
+    size = len(list_of_integers)
+    for i in range(size):
         if i == 0:
             prev_n = 0
         else:
             prev_n = list_of_integers[i - 1]
-        if i == len(list_of_integers) - 1:
+        if i == size - 1:
             next_n = 0
         else:
             next_n = list_of_integers[i + 1]
