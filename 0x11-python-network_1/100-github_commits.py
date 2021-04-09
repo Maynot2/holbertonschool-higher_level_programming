3e96626653ae4ed087877c9fcc0bba47f34b8586
Maynot2 <paulmanot@gmail.com>
2021-04-09 16:43:05 +0000

    Fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-    list 10 commits (from the most recent to oldest) of the repository “rails”
-    by the user “rails”
-"""
-
+"""Fetches the 10 latest commits from a given repo"""
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
 
-if __name__ == '__main__':
-    sha = argv[1]
-    repo = argv[2]
     h = {'Accept': 'application/vnd.github.v3+json'}
-    u = 'https://api.github.com/repos/{}/{}/commits'.format(sha, repo)
-    resp = requests.get(url=u, params={'per_page': 10}, headers=h)
-    json = resp.json()
+    u = "http://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = requests.get(url=u, params={'per_page': 10}, headers=h)
+    json = response.json()
     for commit in json:
-        print('{}: {}'.format(commit.get('sha'),
-              commit.get('commit').get('author').get('name')))
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
