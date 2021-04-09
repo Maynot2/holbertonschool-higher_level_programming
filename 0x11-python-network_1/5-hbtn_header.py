2c97b87ac30e264f5cec5cea691bffddbb919c6a
Maynot2 <paulmanot@gmail.com>
2021-04-09 11:56:50 +0000

    Display header var X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Fetches https://intranet.hbtn.io/status using requests package
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    resp = requests.get(url)
+    print(resp.headers['x-request-id'])
