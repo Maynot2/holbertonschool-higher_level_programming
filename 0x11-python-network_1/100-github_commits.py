82b31f1e0849a6777b512ff2923e3eae45f2b541
Maynot2 <paulmanot@gmail.com>
2021-04-09 16:16:15 +0000

    Create script

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    sends a POST request to http://0.0.0.0:5000/search_user with a letter as
+    parameter.
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    sha = argv[1]
+    repo = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sha, repo)
+    resp = requests.get(url, params={'per_page': 10})
+    try:
+        json = resp.json()
+        for commit in json:
+            print('{}: {}'.format(commit['sha'],
+                  commit['commit']['author']['name']))
+    except:
+        print(None)
