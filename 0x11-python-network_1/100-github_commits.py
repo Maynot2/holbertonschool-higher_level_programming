c1eb9e9251a524c18f1c22671f7c5d381d67f584
Maynot2 <paulmanot@gmail.com>
2021-04-09 16:38:30 +0000

    Add header

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    sends a POST request to http://0.0.0.0:5000/search_user with a letter as
-    parameter.
+    list 10 commits (from the most recent to oldest) of the repository “rails”
+    by the user “rails”
 """
 
 import requests
@@ -10,8 +10,9 @@ from sys import argv
 if __name__ == '__main__':
     sha = argv[1]
     repo = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sha, repo)
-    resp = requests.get(url, params={'per_page': 10})
+    h = {'Accept': 'application/vnd.github.v3+json'}
+    u = 'https://api.github.com/repos/{}/{}/commits'.format(sha, repo)
+    resp = requests.get(url=u, params={'per_page': 10}, headers=h)
     json = resp.json()
     for commit in json:
         print('{}: {}'.format(commit.get('sha'),
