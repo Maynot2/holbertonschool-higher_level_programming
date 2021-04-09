2f4b21de6d77f533f711870352bb1de75ea38c5b
Maynot2 <paulmanot@gmail.com>
2021-04-09 13:54:29 +0000

    Try something else

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
     user = argv[1]
     passwd = argv[2]
     url = 'https://api.github.com/users/{}'.format(user)
-    pars = {'token': passwd}
-    resp = requests.get(url, params=pars)
+    resp = requests.get(url, auth=(user, passwd))
     try:
         json = resp.json()
         print('{}'.format(json['id']))
