bc069f7493cf4b03f91cff2b88a2662f5ec8a105
Maynot2 <paulmanot@gmail.com>
2021-04-09 12:17:18 +0000

    Send POST request with params

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Fetches https://intranet.hbtn.io/status using requests package
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = argv[2]
+    params = {'email': email}
+    resp = requests.post(url, params)
+    print(resp.text)
