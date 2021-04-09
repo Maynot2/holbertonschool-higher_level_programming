e673c9927ea1f203711e92c185cfb7719b98c50e
Maynot2 <paulmanot@gmail.com>
2021-04-09 10:19:19 +0000

    Remove herdoc

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+            print(response.read().decode('utf-8'))
 """
     Fetches https://intranet.hbtn.io/status
 """
@@ -9,10 +10,7 @@ from urllib import request
 if __name__ == '__main__':
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         resp = response.read()
-        content = resp.decode('utf-8')
-        body = '''\
-Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}'''.format(type(resp), resp, content)
-        print(body)
+        print('Body response:')
+        print('\t- type: {}'.format(type(resp)))
+        print('\t- content: {}'.format(resp))
+        print('\t- utf8 content: {}'.format(resp.decode('utf-8')))
