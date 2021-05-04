de3f62fb08242571d412efa65ee3af9f3e7f0369
Maynot2 <paulmanot@gmail.com>
2021-05-04 08:39:28 +0000

    Add script to querry given url and print response status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2] || '';
+
+request(url, function handleResponse (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Code : ${res.statusCode}`);
+  }
+});
