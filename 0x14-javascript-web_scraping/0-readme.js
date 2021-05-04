d49a3ddd2d3e4e314c6f2b519bfec620e027b05f
Maynot2 <paulmanot@gmail.com>
2021-05-04 08:22:24 +0000

    Change script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
+const file = process.argv[2] || '';
 const encryption = 'utf8';
 
-try {
-  const fileContent = fs.readFileSync(file, encryption);
-  console.log(fileContent);
-} catch (err) {
-  console.log(err);
-}
+fs.readFile(
+  file,
+  encryption,
+  function handleError (err, content) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(content);
+    }
+  }
+);
