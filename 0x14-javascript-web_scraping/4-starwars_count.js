6299b21ac486a74fece1e791be6db6c317d61e17
Maynot2 <paulmanot@gmail.com>
2021-05-04 10:50:37 +0000

    Use right url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = process.argv[2];
+const characterId = '18';
 
 request(url, function handleResponse (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    const character = JSON.parse(body);
-    console.log(character.films.length);
+    let counter = 0;
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes(characterId)) {
+          counter++;
+        }
+      }
+    }
+    console.log(counter);
   }
 });
