84999888ea30cdd57c606b32d8b4e129d89f1918
Maynot2 <paulmanot@gmail.com>
2021-05-04 08:25:02 +0000

    Fix indentatation

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,15 +2,17 @@
 
 const fs = require('fs');
 const filePath = process.argv[2] || '';
-const content = process.argv[3]
+const content = process.argv[3];
 const encryption = 'utf8';
 
-fs.writeFile(filePath, content, 
-  { encoding: encryption,
-    flag: 'w'},
-  function handleError(err) {
+fs.writeFile(filePath, content,
+  {
+    encoding: encryption,
+    flag: 'w'
+  },
+  function handleError (err) {
     if (err) {
-      console.log(err.message)
+      console.log(err.message);
     }
   }
 );
